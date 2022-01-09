d789a0112dac080b7eb3e81302a45202d14b699b
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-01-09 14:27:38 +0100

    Removed dist

--- dist/constants/TABLE_NAMES.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const TABLE_NAMES = {
-    PRODUCTS: 'products'
-};
-exports.default = TABLE_NAMES;
--- dist/constants/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.TABLE_NAMES = void 0;
-var TABLE_NAMES_1 = require("./TABLE_NAMES");
-Object.defineProperty(exports, "TABLE_NAMES", { enumerable: true, get: function () { return __importDefault(TABLE_NAMES_1).default; } });
