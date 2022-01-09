43d95e81261fadad7d2fd2f5eb45151de5c094be
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-01-09 13:49:52 +0100

    Added /dist

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.multiplyBy2 = void 0;
+const multiplyBy2 = (num) => {
+    const result = num * 2;
+    return result;
+};
+exports.multiplyBy2 = multiplyBy2;
