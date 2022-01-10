e4cb31b624577c2975e3d6340781483442eb84d2
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-01-11 00:54:07 +0100

    Added more table names

--- src/constants/DB_TABLES.ts
+++ src/constants/DB_TABLES.ts
@@ -1,9 +1,21 @@
 const DB_TABLES = {
-  PRODUCTS: 'products' as const,
-  IMAGES: 'images' as const,
+  ADDRESSES: 'addresses' as const,
+  CALLBACK: 'callbacks' as const,
+  COLLECTIONS: 'collections' as const,
+  DELIVERY_TYPES: 'delivery_types' as const,
   ICONS: 'icons' as const,
+  IMAGES: 'images' as const,
+  INVOICES: 'invoices' as const,
   LABELS: 'labels' as const,
-  COLLECTIONS: 'collections' as const
+  MOLDS: 'molds' as const,
+  NEWSLETTER: 'newsletters' as const,
+  ORDER: 'orders' as const,
+  ORDER_ITEMS: 'order_items' as const,
+  PARCELS: 'parcels' as const,
+  PAYMENT_TYPE: 'payment_types' as const,
+  PRODUCTS: 'products' as const,
+  USERS: 'users' as const,
+  VOUCHERS: 'vouchers' as const
 }
 
 export default DB_TABLES
