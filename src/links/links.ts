806475e0acc92f85327ad8c7788da83ed86e2630
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-01-11 17:33:31 +0100

    v1.0.49

--- src/links/links.ts
+++ src/links/links.ts
@@ -1,7 +1,7 @@
 import { WEBISTE_URL, BROWSER_ROUTER_SUFFIX } from './index'
 import ROUTES from './ROUTES'
 import { SEPARATORS } from '../constants'
-import { Product, Order, order } from '..'
+import { Product, Order } from '..'
 
 export const getViewProductLink = (productId: Product['id']) =>
   `${WEBISTE_URL}${BROWSER_ROUTER_SUFFIX}${ROUTES.PRODUCTS}${SEPARATORS.PATH}${productId}` as const
