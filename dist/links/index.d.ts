36e023cf404e2f66193c5a2bde604a55e9e51e4d
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-01-11 17:33:24 +0100

    Misc

--- dist/links/index.d.ts
+++ dist/links/index.d.ts
@@ -1,4 +1,4 @@
 export declare const WEBISTE_URL: "https://braty.co";
-export declare const BROWSER_ROUTER_SUFFIX = "/#";
+export declare const BROWSER_ROUTER_SUFFIX: "/#";
 export { default as ROUTES } from './ROUTES';
 export * from './links';
--- src/links/index.ts
+++ src/links/index.ts
@@ -1,5 +1,5 @@
 export const WEBISTE_URL = 'https://braty.co' as const
-export const BROWSER_ROUTER_SUFFIX = '/#'
+export const BROWSER_ROUTER_SUFFIX = '/#' as const
 
 export { default as ROUTES } from './ROUTES'
 export * from './links'
