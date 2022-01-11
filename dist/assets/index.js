de3bdfa0a0dbad2faa7ff59cd2b4665768d2bbb6
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-01-11 18:08:52 +0100

    Typo

--- dist/assets/index.js
+++ dist/assets/index.js
@@ -1,4 +1,4 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.BRATY_LOGO_ = void 0;
-exports.BRATY_LOGO_ = 'https://firebasestorage.googleapis.com/v0/b/zapalniczki-c7234.appspot.com/o/assets%2Fbraty_logo.png?alt=media&token=2f5017c8-b8ad-4b29-af3f-d6b8b18bd1d2';
+exports.BRATY_LOGO = void 0;
+exports.BRATY_LOGO = 'https://firebasestorage.googleapis.com/v0/b/zapalniczki-c7234.appspot.com/o/assets%2Fbraty_logo.png?alt=media&token=2f5017c8-b8ad-4b29-af3f-d6b8b18bd1d2';
