4676b937557f6c8fa092bf511ca2ea378407e047
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-01-12 00:21:06 +0100

    Build

--- dist/dbModels/marketing.js
+++ dist/dbModels/marketing.js
@@ -1,8 +1,11 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const zod_1 = require("zod");
-const _1 = require(".");
-const marketing = _1.tableBase.extend({
+const tableBase_1 = __importDefault(require("./tableBase"));
+const marketing = tableBase_1.default.extend({
     email: (0, zod_1.string)().optional(),
     phone: (0, zod_1.string)().optional(),
     notes: (0, zod_1.string)().optional()
