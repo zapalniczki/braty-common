d4420f49660a2b1d0722d4efe2dd67142665a4b4
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-01-11 18:02:55 +0100

    Added more constants

---
+++ src/assets/index.ts
@@ -0,0 +1,2 @@
+export const BRATY_LOGO_ =
+  'https://firebasestorage.googleapis.com/v0/b/zapalniczki-c7234.appspot.com/o/assets%2Fbraty_logo.png?alt=media&token=2f5017c8-b8ad-4b29-af3f-d6b8b18bd1d2'
---
+++ src/info/index.ts
@@ -0,0 +1,9 @@
+export const BRATY_PHONE = '+48 792 531 179'
+
+export const BRATY_EMAIL = 'kontakt@braty.co'
+
+export const BRATY_ADDRESS_1 = 'Braty Przemysław Rychlewicz'
+export const BRATY_ADDRESS_2 = 'ul. Bora-Komorowskiego 12/1'
+export const BRATY_ADDRESS_3 = '80-366 Gdańsk'
+
+export const BRATY_NAME = 'braty.co'
