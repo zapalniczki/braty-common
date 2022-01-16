bcee7b0f93dc89d6001f9cbe96ce43362ee4270b
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-01-16 12:36:06 +0100

    Updated collection model

--- src/dbModels/collection.ts
+++ src/dbModels/collection.ts
@@ -4,7 +4,8 @@ import { boolean, string, TypeOf } from 'zod'
 const collection = tableBase.extend({
   label_pl: string(),
   label_en: string(),
-  visible: boolean()
+  visible: boolean(),
+  featured: boolean()
 })
 
 export type Collection = TypeOf<typeof collection>
