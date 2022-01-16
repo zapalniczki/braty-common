600ea355af0c505a178212666a2bd862ec016266
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-01-16 16:54:25 +0100

    Build

--- dist/constants/index.d.ts
+++ dist/constants/index.d.ts
@@ -1,3 +1,4 @@
 export { default as DB_TABLES } from './DB_TABLES';
 export { default as EXTENSIONS } from './EXTENSIONS';
 export { default as SEPARATORS } from './SEPARATORS';
+export { default as LINKS_PREFIXES } from './LINKS_PREFIXES';
--- dist/constants/index.js
+++ dist/constants/index.js
@@ -3,10 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.SEPARATORS = exports.EXTENSIONS = exports.DB_TABLES = void 0;
+exports.LINKS_PREFIXES = exports.SEPARATORS = exports.EXTENSIONS = exports.DB_TABLES = void 0;
 var DB_TABLES_1 = require("./DB_TABLES");
 Object.defineProperty(exports, "DB_TABLES", { enumerable: true, get: function () { return __importDefault(DB_TABLES_1).default; } });
 var EXTENSIONS_1 = require("./EXTENSIONS");
 Object.defineProperty(exports, "EXTENSIONS", { enumerable: true, get: function () { return __importDefault(EXTENSIONS_1).default; } });
 var SEPARATORS_1 = require("./SEPARATORS");
 Object.defineProperty(exports, "SEPARATORS", { enumerable: true, get: function () { return __importDefault(SEPARATORS_1).default; } });
+var LINKS_PREFIXES_1 = require("./LINKS_PREFIXES");
+Object.defineProperty(exports, "LINKS_PREFIXES", { enumerable: true, get: function () { return __importDefault(LINKS_PREFIXES_1).default; } });
