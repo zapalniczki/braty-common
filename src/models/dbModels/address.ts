627f0b7b99d2c06f0670cb1a94e683165100aafc
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-01-19 20:48:17 +0100

    Country as enum

--- src/models/dbModels/address.ts
+++ src/models/dbModels/address.ts
@@ -1,11 +1,14 @@
 import tableBase from './tableBase'
-import { string, TypeOf } from 'zod'
+import { string, TypeOf, enum as zenum } from 'zod'
+
+const country = zenum(['POLAND', 'UNITED KINGDOM', 'GERMANY'])
+export type Country = TypeOf<typeof country>
 
 const address = tableBase.extend({
   city: string(),
   post_code: string(),
   street_address: string(),
-  country: string()
+  country
 })
 
 export type Address = TypeOf<typeof address>
