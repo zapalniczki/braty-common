14841b405df69ba386f09c5e912e6cfbf2a1a437
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-01-19 20:48:24 +0100

    Build

--- dist/models/dbModels/address.d.ts
+++ dist/models/dbModels/address.d.ts
@@ -1,4 +1,6 @@
 import { TypeOf } from 'zod';
+declare const country: import("zod").ZodEnum<["POLAND", "UNITED KINGDOM", "GERMANY"]>;
+export declare type Country = TypeOf<typeof country>;
 declare const address: import("zod").ZodObject<import("zod").extendShape<{
     created_at: import("zod").ZodString;
     id: import("zod").ZodString;
@@ -7,7 +9,7 @@ declare const address: import("zod").ZodObject<import("zod").extendShape<{
     city: import("zod").ZodString;
     post_code: import("zod").ZodString;
     street_address: import("zod").ZodString;
-    country: import("zod").ZodString;
+    country: import("zod").ZodEnum<["POLAND", "UNITED KINGDOM", "GERMANY"]>;
 }>, "strip", import("zod").ZodTypeAny, {
     created_at: string;
     id: string;
@@ -15,7 +17,7 @@ declare const address: import("zod").ZodObject<import("zod").extendShape<{
     city: string;
     post_code: string;
     street_address: string;
-    country: string;
+    country: "POLAND" | "UNITED KINGDOM" | "GERMANY";
 }, {
     created_at: string;
     id: string;
@@ -23,7 +25,7 @@ declare const address: import("zod").ZodObject<import("zod").extendShape<{
     city: string;
     post_code: string;
     street_address: string;
-    country: string;
+    country: "POLAND" | "UNITED KINGDOM" | "GERMANY";
 }>;
 export declare type Address = TypeOf<typeof address>;
 export default address;
--- dist/models/dbModels/address.js
+++ dist/models/dbModels/address.js
@@ -5,10 +5,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var tableBase_1 = __importDefault(require("./tableBase"));
 var zod_1 = require("zod");
+var country = (0, zod_1.enum)(['POLAND', 'UNITED KINGDOM', 'GERMANY']);
 var address = tableBase_1.default.extend({
     city: (0, zod_1.string)(),
     post_code: (0, zod_1.string)(),
     street_address: (0, zod_1.string)(),
-    country: (0, zod_1.string)()
+    country: country
 });
 exports.default = address;
