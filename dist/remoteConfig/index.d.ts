944b47795fefbaebfe6ab9c2bec7866e605295b7
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-01-20 10:31:33 +0100

    Build

--- dist/remoteConfig/index.d.ts
+++ dist/remoteConfig/index.d.ts
@@ -1,1 +1,1 @@
-export {};
+export { default as getTypeOfRemoteConfigKey } from './getTypeOfRemoteConfigKey';
--- dist/remoteConfig/index.js
+++ dist/remoteConfig/index.js
@@ -1,2 +1,8 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.getTypeOfRemoteConfigKey = void 0;
+var getTypeOfRemoteConfigKey_1 = require("./getTypeOfRemoteConfigKey");
+Object.defineProperty(exports, "getTypeOfRemoteConfigKey", { enumerable: true, get: function () { return __importDefault(getTypeOfRemoteConfigKey_1).default; } });
