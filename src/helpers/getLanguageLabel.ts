7027d925925a154ac748916fa871d0af99cbb29f
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-01-26 00:12:51 +0100

    Added getLanguageLabel

---
+++ src/helpers/getLanguageLabel.ts
@@ -0,0 +1,56 @@
+import { Language } from '..'
+
+type Props = {
+  language: Language
+} & (
+  | {
+      description?: false
+      label: Name
+      name: true
+    }
+  | {
+      description: true
+      label: Description
+      name?: false
+    }
+  | {
+      description?: false
+      label: Label
+      name?: false
+    }
+)
+
+function getLanguageLabel(props: Props) {
+  if (props.description) {
+    const key = `description_${props.language}` as const
+
+    return props.label[key]
+  }
+
+  if (props.name) {
+    const key = `name_${props.language}` as const
+
+    return props.label[key]
+  }
+
+  const key = `label_${props.language}` as const
+
+  return props.label[key]
+}
+
+export default getLanguageLabel
+
+type Label = {
+  label_en: string
+  label_pl: string
+}
+
+type Description = {
+  description_en: string
+  description_pl: string
+}
+
+type Name = {
+  name_en: string
+  name_pl: string
+}
--- src/helpers/index.ts
+++ src/helpers/index.ts
@@ -1,1 +1,2 @@
 export { default as getObjectEntries } from './getObjectEntries'
+export { default as getLanguageLabel } from './getLanguageLabel'
