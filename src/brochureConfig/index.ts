8c3270201a8b6403965d406db7c1666f8bb819b6
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-01-26 00:55:46 +0100

    Added i18n

--- src/brochureConfig/index.ts
+++ src/brochureConfig/index.ts
@@ -1,12 +1,43 @@
-export const BROCHURE_TITLE = 'Katalog produktów' as const
-export const BROCHURE_SUBTITLE = 'Zima 2021 | 22' as const
-export const BROCHURE_CAPTION = 'Zapoznaj się z aktualną ofertą' as const
+import { Language } from '..'
 
-export const FEATURED_SUBTITLE = 'Oferta specjalna'
-export const REGULAR_SUBTITLE = 'Oferta standardowa'
+export const BROCHURE_TITLE: Record<Language, string> = {
+  pl: 'Katalog produktów' as const,
+  en: 'Product Catalogue' as const
+}
 
-export const TILE_TITLE_PREFIX = 'Zapalniczka ' as const
+export const BROCHURE_SUBTITLE: Record<Language, string> = {
+  pl: 'Zima 2021 | 22' as const,
+  en: 'Winter 2021 | 22' as const
+}
+
+export const BROCHURE_CAPTION: Record<Language, string> = {
+  pl: 'Zapoznaj się z aktualną ofertą' as const,
+  en: 'Browse our wholesale offer' as const
+}
+
+export const FEATURED_SUBTITLE: Record<Language, string> = {
+  pl: 'Oferta specjalna',
+  en: 'Featured products'
+}
+
+export const REGULAR_SUBTITLE: Record<Language, string> = {
+  pl: 'Oferta standardowa',
+  en: 'Regular products'
+}
+
+export const TILE_TITLE_PREFIX: Record<Language, string> = {
+  pl: 'Zapalniczka ' as const,
+  en: 'Lighter ' as const
+}
+
 export const PRODUCT_ICON_ONLY_LABEL = '-' as const
-export const PRODUCT_NAME_FOR_ICON_ONLY = 'Zapalniczki tylko z ikonami' as const
 
-export const TOC_TITLE = 'Spis treści' as const
+export const PRODUCT_NAME_FOR_ICON_ONLY: Record<Language, string> = {
+  pl: 'Zapalniczki tylko z ikonami' as const,
+  en: 'Zapalniczki tylko z ikonami' as const
+}
+
+export const TOC_TITLE = {
+  pl: 'Spis treści' as const,
+  en: 'Table of Contents' as const
+}
