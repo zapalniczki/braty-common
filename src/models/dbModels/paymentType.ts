8bc2a5f42c6b4295b1bd86d658f23b207b3d7282
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-01-26 10:46:02 +0100

    Updated models

--- src/models/dbModels/paymentType.ts
+++ src/models/dbModels/paymentType.ts
@@ -4,12 +4,14 @@ import international from './international'
 import iconName from './iconName'
 
 const paymentType = tableBase.merge(international).extend({
-  price: number(),
-  description_pl: string(),
+  abroad: boolean(),
   description_en: string(),
+  description_pl: string(),
+  domestic: boolean(),
   icon_name: iconName,
+  in_person: boolean(),
   is_enabled: boolean(),
-  in_person: boolean()
+  price: number()
 })
 
 export type PaymentType = TypeOf<typeof paymentType>
