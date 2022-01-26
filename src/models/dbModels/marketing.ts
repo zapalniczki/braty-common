f1112c028f03a453131276f2b217a9a772d8d8f1
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-01-26 19:47:11 +0100

    Updated model

--- src/models/dbModels/marketing.ts
+++ src/models/dbModels/marketing.ts
@@ -1,4 +1,4 @@
-import { boolean, string, TypeOf } from 'zod'
+import { boolean, string, TypeOf, enum as zenum } from 'zod'
 import tableBase from './tableBase'
 
 const marketing = tableBase.extend({
@@ -9,7 +9,8 @@ const marketing = tableBase.extend({
   phone: string().nullable(),
   plus_code: string().nullable(),
   send_brochure_agreement: boolean(),
-  send_brochure_cyclic_agreement: boolean()
+  send_brochure_cyclic_agreement: boolean(),
+  status: zenum(['REJECTED', 'TODO', 'INTERESTED', 'UNANSWERED'])
 })
 
 export type Marketing = TypeOf<typeof marketing>
