f342faba7ebe297ed4acc875f21d77035463a6ed
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-01-28 22:54:46 +0100

    Mix

--- src/configs/catalogue.ts
+++ src/configs/catalogue.ts
@@ -1,43 +1,43 @@
 import { Language } from '..'
 
-export const BROCHURE_TITLE: Record<Language, string> = {
+export const CATALOGUE_TITLE: Record<Language, string> = {
   pl: 'Katalog produktów' as const,
   en: 'Products Catalogue' as const
 }
 
-export const BROCHURE_SUBTITLE: Record<Language, string> = {
+export const CATALOGUE_SUBTITLE: Record<Language, string> = {
   pl: 'Zima 2021 | 22' as const,
   en: 'Winter 2021 | 22' as const
 }
 
-export const BROCHURE_CAPTION: Record<Language, string> = {
+export const CATALOGUE_CAPTION: Record<Language, string> = {
   pl: 'Zapoznaj się z aktualną ofertą' as const,
   en: 'Browse our wholesale offer' as const
 }
 
-export const FEATURED_SUBTITLE: Record<Language, string> = {
+export const CATALOGUE_FEATURED_SUBTITLE: Record<Language, string> = {
   pl: 'Oferta specjalna',
   en: 'Featured products'
 }
 
-export const REGULAR_SUBTITLE: Record<Language, string> = {
+export const CATALOGUE_REGULAR_SUBTITLE: Record<Language, string> = {
   pl: 'Oferta standardowa',
   en: 'Regular products'
 }
 
-export const TILE_TITLE_PREFIX: Record<Language, string> = {
+export const CATALOGUE_TILE_TITLE_PREFIX: Record<Language, string> = {
   pl: 'Zapalniczka ' as const,
   en: 'Lighter ' as const
 }
 
-export const PRODUCT_ICON_ONLY_LABEL = '-' as const
+export const CATALOGUE_PRODUCT_ICON_ONLY_LABEL = '-' as const
 
-export const PRODUCT_NAME_FOR_ICON_ONLY: Record<Language, string> = {
+export const CATALOGUE_PRODUCT_NAME_FOR_ICON_ONLY: Record<Language, string> = {
   pl: 'Zapalniczki tylko z ikonami' as const,
   en: 'Zapalniczki tylko z ikonami' as const
 }
 
-export const TOC_TITLE = {
+export const CATALOGUE_TOC_TITLE = {
   pl: 'Spis treści' as const,
   en: 'Table of Contents' as const
 }
---
+++ src/constants/SCRIPTS.ts
@@ -0,0 +1,6 @@
+const SCRIPTS = {
+  PHOTOS: 'braty-photos' as const,
+  CATALOGUE: 'braty-catalogue' as const
+}
+
+export default SCRIPTS
