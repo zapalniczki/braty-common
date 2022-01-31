caea773241a909eee02386ccace4e89264a8d273
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-01-31 18:10:58 +0100

    Build

--- dist/configs/index.d.ts
+++ dist/configs/index.d.ts
@@ -1,3 +1,4 @@
 export * from './catalogue';
 export * from './photos';
 export * from './storage';
+export declare const BRATY_BASE_PATH: "C:/Users/Przemek Rychlewicz/BRATY";
--- dist/configs/index.js
+++ dist/configs/index.js
@@ -10,6 +10,8 @@ var __exportStar = (this && this.__exportStar) || function(m, exports) {
     for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.BRATY_BASE_PATH = void 0;
 __exportStar(require("./catalogue"), exports);
 __exportStar(require("./photos"), exports);
 __exportStar(require("./storage"), exports);
+exports.BRATY_BASE_PATH = 'C:/Users/Przemek Rychlewicz/BRATY';
