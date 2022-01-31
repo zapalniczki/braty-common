bcbfd0038f0273e51a19ac191c17cd97d50b126f
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-01-31 17:06:52 +0100

    Misc

--- src/constants/SEPARATORS.ts
+++ src/constants/SEPARATORS.ts
@@ -1,6 +1,7 @@
 const SEPARATORS = {
   PATH: '/' as const,
   FILENAME: '&' as const,
+  FILENAME_PAIR: '=' as const,
   PRODUCT_NAME: ', ' as const,
   SPACE: ' ' as const
 }
---
+++ src/helpers/parseFilename.ts
@@ -0,0 +1,44 @@
+import { SEPARATORS } from '..'
+
+function parseFilename(filename: string) {
+  const parts = filename.split(SEPARATORS.FILENAME)
+
+  const keys = []
+
+  for (let index = 0; index < parts.length; index++) {
+    const part = parts[index]
+    const partParts = part.split(SEPARATORS.FILENAME_PAIR)
+    const partObject = {
+      key: partParts[0],
+      value: partParts[1]
+    }
+
+    keys.push(partObject)
+  }
+
+  let collection
+  let icon
+  let label
+  let size
+
+  for (let index = 0; index < keys.length; index++) {
+    const key = keys[index]
+
+    if (key.key === 'collection') {
+      collection = key.value
+    } else if (key.key === 'label') {
+      label = key.value
+    } else if (key.key === 'icon') {
+      icon = key.value
+    }
+  }
+
+  return {
+    collection,
+    icon,
+    label,
+    size
+  }
+}
+
+export default parseFilename
