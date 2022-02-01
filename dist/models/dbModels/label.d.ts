0d5399816307c307d6ee9462faf847660f4dc8af
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-02-01 14:08:44 +0100

    Build

--- dist/models/dbModels/label.d.ts
+++ dist/models/dbModels/label.d.ts
@@ -7,6 +7,7 @@ declare const label: import("zod").ZodObject<import("zod").extendShape<import("z
     label_pl: import("zod").ZodString;
     label_en: import("zod").ZodString;
 }>, {
+    collection_id: import("zod").ZodString;
     icons: import("zod").ZodArray<import("zod").ZodEnum<["PKiN", "HEART_2022", "DOUBLE_HEART", "ARROW_BELOW", "CHRISTMAS_TREE_STRIPES", "CHRISTMAS_TREE_STAR", "CHRISTMAS_TREE_PATTERN", "BALLOONS", "FIREWORKS", "SANTA_FACE", "CHRISTMAS_TREE", "SHIP", "WARSAW_MERMAID", "BOAT2", "HEART"]>, "many">;
 }>, "strip", import("zod").ZodTypeAny, {
     icons: ("PKiN" | "HEART_2022" | "DOUBLE_HEART" | "ARROW_BELOW" | "CHRISTMAS_TREE_STRIPES" | "CHRISTMAS_TREE_STAR" | "CHRISTMAS_TREE_PATTERN" | "BALLOONS" | "FIREWORKS" | "SANTA_FACE" | "CHRISTMAS_TREE" | "SHIP" | "WARSAW_MERMAID" | "BOAT2" | "HEART")[];
@@ -15,6 +16,7 @@ declare const label: import("zod").ZodObject<import("zod").extendShape<import("z
     created_at: string;
     id: string;
     updated_at: string;
+    collection_id: string;
 }, {
     icons: ("PKiN" | "HEART_2022" | "DOUBLE_HEART" | "ARROW_BELOW" | "CHRISTMAS_TREE_STRIPES" | "CHRISTMAS_TREE_STAR" | "CHRISTMAS_TREE_PATTERN" | "BALLOONS" | "FIREWORKS" | "SANTA_FACE" | "CHRISTMAS_TREE" | "SHIP" | "WARSAW_MERMAID" | "BOAT2" | "HEART")[];
     label_pl: string;
@@ -22,6 +24,7 @@ declare const label: import("zod").ZodObject<import("zod").extendShape<import("z
     created_at: string;
     id: string;
     updated_at: string;
+    collection_id: string;
 }>;
 export declare type Label = TypeOf<typeof label>;
 export default label;
--- dist/models/dbModels/label.js
+++ dist/models/dbModels/label.js
@@ -8,6 +8,7 @@ var __1 = require("..");
 var international_1 = __importDefault(require("./international"));
 var tableBase_1 = __importDefault(require("./tableBase"));
 var label = tableBase_1.default.merge(international_1.default).extend({
+    collection_id: (0, zod_1.string)().uuid(),
     icons: (0, zod_1.array)(__1.iconKey)
 });
 exports.default = label;
