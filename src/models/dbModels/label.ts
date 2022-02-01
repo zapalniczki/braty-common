4b80337774ae132810218d420fb96399bd11c9e2
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-02-01 02:39:59 +0100

    mic

--- src/models/dbModels/label.ts
+++ src/models/dbModels/label.ts
@@ -1,8 +1,11 @@
-import tableBase from './tableBase'
-import { string, TypeOf } from 'zod'
+import { array, TypeOf } from 'zod'
+import { iconKey } from '..'
 import international from './international'
+import tableBase from './tableBase'
 
-const label = tableBase.merge(international)
+const label = tableBase.merge(international).extend({
+  icons: array(iconKey)
+})
 
 export type Label = TypeOf<typeof label>
 
