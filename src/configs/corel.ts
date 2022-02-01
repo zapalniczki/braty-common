fd2d2decb8397720b3e62a097802fe44f495dee3
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-02-01 15:10:17 +0100

    COrel

--- src/configs/corel.ts
+++ src/configs/corel.ts
@@ -1,5 +1,30 @@
 export const COREL_PATHS = {
-  output: '/output' as const,
-  constants_path: '/src/constants.ts' as const,
-  config_path: '/src/config.ts' as const
+  CONFIG_PATH: '/src/config.ts' as const,
+  CONSTANTS_PATH: '/src/constants.ts' as const,
+  ICONS: '/icons' as const,
+  OUTPUT: '/output' as const
 }
+
+export const COREL_MARGINS_BETWEEN_ICON_AND_TEXT = {
+  SMALL: 0.05,
+  LARGE: 0.15
+}
+
+export const COREL_FONTS: Record<CorelFont, CorelFontFamily> = {
+  STANDARD: 'Kaushan Script'
+}
+
+export const COREL_TEXT_LAYER_NAME = 'text_layer'
+export const COREL_ICON_LAYER_NAME = 'icon_layer'
+
+export const COREL_LABEL_PREFIX = 'I'
+
+export const COREL_ICON_LAYER_INITIAL_NAME = 'Warstwa 1'
+
+export type CorelIcon = 'HEART' | 'BOAT'
+
+export type Unicode = number[]
+
+export type CorelFontFamily = 'Kaushan Script' | 'Batang'
+
+export type CorelFont = 'STANDARD'
--- src/configs/storage.ts
+++ src/configs/storage.ts
@@ -1,4 +1,4 @@
 export const LOG_COLORS = {
-  green: '\x1b[32m%s\x1b[0m' as const,
-  red: '\x1b[31m%s\x1b[0m' as const
+  GREEN: '\x1b[32m%s\x1b[0m' as const,
+  RED: '\x1b[31m%s\x1b[0m' as const
 }
