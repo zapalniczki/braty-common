fba9d8d508a357cc5f2c2d2be1f79361ac84252b
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-02-01 02:40:05 +0100

    Build

--- dist/models/dbModels/label.d.ts
+++ dist/models/dbModels/label.d.ts
@@ -1,18 +1,22 @@
 import { TypeOf } from 'zod';
-declare const label: import("zod").ZodObject<import("zod").extendShape<{
+declare const label: import("zod").ZodObject<import("zod").extendShape<import("zod").extendShape<{
     created_at: import("zod").ZodString;
     id: import("zod").ZodString;
     updated_at: import("zod").ZodString;
 }, {
     label_pl: import("zod").ZodString;
     label_en: import("zod").ZodString;
+}>, {
+    icons: import("zod").ZodArray<import("zod").ZodEnum<["PKiN", "HEART_2022", "DOUBLE_HEART", "ARROW_BELOW", "CHRISTMAS_TREE_STRIPES", "CHRISTMAS_TREE_STAR", "CHRISTMAS_TREE_PATTERN", "BALLOONS", "FIREWORKS", "SANTA_FACE", "CHRISTMAS_TREE", "SHIP", "WARSAW_MERMAID", "BOAT2", "HEART"]>, "many">;
 }>, "strip", import("zod").ZodTypeAny, {
+    icons: ("PKiN" | "HEART_2022" | "DOUBLE_HEART" | "ARROW_BELOW" | "CHRISTMAS_TREE_STRIPES" | "CHRISTMAS_TREE_STAR" | "CHRISTMAS_TREE_PATTERN" | "BALLOONS" | "FIREWORKS" | "SANTA_FACE" | "CHRISTMAS_TREE" | "SHIP" | "WARSAW_MERMAID" | "BOAT2" | "HEART")[];
     label_pl: string;
     label_en: string;
     created_at: string;
     id: string;
     updated_at: string;
 }, {
+    icons: ("PKiN" | "HEART_2022" | "DOUBLE_HEART" | "ARROW_BELOW" | "CHRISTMAS_TREE_STRIPES" | "CHRISTMAS_TREE_STAR" | "CHRISTMAS_TREE_PATTERN" | "BALLOONS" | "FIREWORKS" | "SANTA_FACE" | "CHRISTMAS_TREE" | "SHIP" | "WARSAW_MERMAID" | "BOAT2" | "HEART")[];
     label_pl: string;
     label_en: string;
     created_at: string;
--- dist/models/dbModels/label.js
+++ dist/models/dbModels/label.js
@@ -3,7 +3,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var tableBase_1 = __importDefault(require("./tableBase"));
+var zod_1 = require("zod");
+var __1 = require("..");
 var international_1 = __importDefault(require("./international"));
-var label = tableBase_1.default.merge(international_1.default);
+var tableBase_1 = __importDefault(require("./tableBase"));
+var label = tableBase_1.default.merge(international_1.default).extend({
+    icons: (0, zod_1.array)(__1.iconKey)
+});
 exports.default = label;
