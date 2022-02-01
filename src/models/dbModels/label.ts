a89986df81c3dcd8cf5c6055bb64e0df6526d68d
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-02-01 14:08:32 +0100

    Model

--- src/models/dbModels/label.ts
+++ src/models/dbModels/label.ts
@@ -1,9 +1,10 @@
-import { array, TypeOf } from 'zod'
+import { array, string, TypeOf } from 'zod'
 import { iconKey } from '..'
 import international from './international'
 import tableBase from './tableBase'
 
 const label = tableBase.merge(international).extend({
+  collection_id: string().uuid(),
   icons: array(iconKey)
 })
 
