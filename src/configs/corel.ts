9559a3f4d7c1e7f5503e3dcdf8f4eeb02c930ab1
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-02-01 14:52:54 +0100

    misc

--- src/configs/corel.ts
+++ src/configs/corel.ts
@@ -1,3 +1,5 @@
 export const COREL_PATHS = {
-  output: '/output' as const
+  output: '/output' as const,
+  constants_path: '/src/constants.ts' as const,
+  config_path: '/src/config.ts' as const
 }
