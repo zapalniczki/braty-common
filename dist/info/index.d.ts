e1e9e56d39ab62451e77f8d58cf495233e367430
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-02-05 23:00:54 +0100

    Build

--- dist/info/index.d.ts
+++ dist/info/index.d.ts
@@ -2,7 +2,7 @@ export declare const BRATY_PHONE: "+48 792 531 179";
 export declare const BRATY_EMAIL: "kontakt@braty.co";
 export declare const BRATY_ADDRESS_1: "Braty Przemysław Rychlewicz";
 export declare const BRATY_ADDRESS_2: "ul. Bora-Komorowskiego 12/1";
-export declare const BRATY_ADDRESS_3: "80-366 Gdańsk";
+export declare const BRATY_ADDRESS_3: "80-366 Gdańsk, POLAND";
 export declare const BRATY_NAME: "braty.co";
 export declare const BRATY_IBAN: "PL31 1140 2004 0000 3402 8090 0648";
 export declare const BRATY_BIC: "BREXPLPWMBK";
