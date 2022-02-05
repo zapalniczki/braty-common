87aa79dbba2656e79b7425220fb0c60417205ca9
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-02-05 22:30:41 +0100

    Updated info

--- src/info/index.ts
+++ src/info/index.ts
@@ -7,3 +7,8 @@ export const BRATY_ADDRESS_2 = 'ul. Bora-Komorowskiego 12/1'
 export const BRATY_ADDRESS_3 = '80-366 Gdańsk'
 
 export const BRATY_NAME = 'braty.co'
+
+export const BRATY_IBAN = 'PL31 1140 2004 0000 3402 8090 0648'
+export const BRATY_BIC = 'BREXPLPWMBK'
+export const BRATY_BANK_NAME = 'mBank S.A.'
+export const BRATY_BANK_ACCOUNT = '31 1140 2004 0000 3402 8090 0648'
