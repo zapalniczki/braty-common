88f34d2d3991da311abc337d88113b8cc69ef228
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-02-06 18:22:17 +0100

    Cleanup scripts

--- src/constants/SCRIPTS.ts
+++ src/constants/SCRIPTS.ts
@@ -1,8 +1,13 @@
 const SCRIPTS = {
-  CATALOGUE: 'braty-catalogue' as const,
   COMMON: 'braty-common' as const,
-  COREL: 'braty-corel' as const,
+
+  CATALOGUE_PREPARE: 'braty-catalogue-prepare' as const,
+  CATALOGUE: 'braty-catalogue' as const,
+  CATALOGUE_UPLOAD: 'braty-catalogue-upload' as const,
+
   MOLDS: 'braty-molds' as const,
+  COREL: 'braty-corel' as const,
+
   PHOTOS: 'braty-photos' as const,
   STORAGE: 'braty-storage' as const
 }
