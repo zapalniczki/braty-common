5a052afa461efb35a4c6b36bb182674c42b941c3
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-02-06 12:53:42 +0100

    fix error

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 export * from './assets'
-export * from './configs'
 export * from './constants'
 export * from './helpers'
 export * from './info'
 export * from './links'
 export * from './models'
 export * from './remoteConfig'
+
+//
+export * from './configs'
