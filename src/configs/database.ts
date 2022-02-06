cb994a98e0b3a3613f0fe1d47ba69f6f34c4eb9c
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-02-06 12:48:33 +0100

    Added database config

---
+++ src/configs/database.ts
@@ -0,0 +1,16 @@
+import { SCRIPTS } from '..'
+
+const OUTPUT_BASE_PATH =
+  `C:/Users/Przemek Rychlewicz/AppData/Roaming/Adobe/InDesign/Version 16.0/en_GB/Scripts/Scripts Panel/${SCRIPTS.CATALOGUE}` as const
+
+const OUTPUT_IMAGES_FOLDER = `${OUTPUT_BASE_PATH}/src/externalAssets` as const
+const OUTPUT_PRODUCTS_FILE_PATH = `${OUTPUT_BASE_PATH}/src/index.ts` as const
+const OUTPUT_CONSTANTS_FILE_PATH =
+  `${OUTPUT_BASE_PATH}/src/constants.ts` as const
+
+export const DATABASE_PATHS = {
+  OUTPUT_BASE_PATH,
+  OUTPUT_IMAGES_FOLDER,
+  OUTPUT_PRODUCTS_FILE_PATH,
+  OUTPUT_CONSTANTS_FILE_PATH
+}
--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,6 +1,6 @@
 export * from './catalogue'
 export * from './corel'
+export * from './database'
 export * from './photos'
 export * from './storage'
-
 export const BRATY_BASE_PATH = 'C:/Users/Przemek Rychlewicz/BRATY' as const
