3683bca9bc1a5ee0df04ab5bff3ec27c212473c5
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-02-09 18:05:26 +0100

    Build

--- dist/configs/cataloguePrepare.d.ts
+++ dist/configs/cataloguePrepare.d.ts
@@ -1,6 +1,6 @@
 export declare const CATALOGUE_PREPARE_PATHS: {
     OUTPUT_BASE_PATH: "C:/Users/Przemek Rychlewicz/AppData/Roaming/Adobe/InDesign/Version 16.0/en_GB/Scripts/Scripts Panel/braty-catalogue";
-    OUTPUT_IMAGES_FOLDER: "C:/Users/Przemek Rychlewicz/AppData/Roaming/Adobe/InDesign/Version 16.0/en_GB/Scripts/Scripts Panel/braty-catalogue/src/externalAssets";
-    OUTPUT_PRODUCTS_FILE_PATH: "C:/Users/Przemek Rychlewicz/AppData/Roaming/Adobe/InDesign/Version 16.0/en_GB/Scripts/Scripts Panel/braty-catalogue/src/index.ts";
-    OUTPUT_CONSTANTS_FILE_PATH: "C:/Users/Przemek Rychlewicz/AppData/Roaming/Adobe/InDesign/Version 16.0/en_GB/Scripts/Scripts Panel/braty-catalogue/src/constants.ts";
+    OUTPUT_IMAGES_FOLDER: "/src/externalAssets";
+    OUTPUT_PRODUCTS_FILE_PATH: "/index.ts";
+    OUTPUT_CONSTANTS_FILE_PATH: "/src/constants.ts";
 };
