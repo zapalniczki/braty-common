c8c3ffa184eb293bf6624f2eecf14735270f28ac
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-02-11 16:48:11 +0100

    Build

--- dist/configs/cataloguePrepare.js
+++ dist/configs/cataloguePrepare.js
@@ -3,9 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.CATALOGUE_PREPARE_PATHS = void 0;
 var __1 = require("..");
 var OUTPUT_BASE_PATH = "C:/Users/Przemek Rychlewicz/AppData/Roaming/Adobe/InDesign/Version 16.0/en_GB/Scripts/Scripts Panel/".concat(__1.SCRIPTS.CATALOGUE);
-var OUTPUT_IMAGES_FOLDER = "/src/externalAssets";
-var OUTPUT_PRODUCTS_FILE_PATH = "/index.ts";
-var OUTPUT_CONSTANTS_FILE_PATH = "/src/constants.ts";
+var OUTPUT_IMAGES_FOLDER = '/src/externalAssets';
+var OUTPUT_PRODUCTS_FILE_PATH = '/src/index.ts';
+var OUTPUT_CONSTANTS_FILE_PATH = '/src/constants.ts';
 exports.CATALOGUE_PREPARE_PATHS = {
     OUTPUT_BASE_PATH: OUTPUT_BASE_PATH,
     OUTPUT_IMAGES_FOLDER: OUTPUT_IMAGES_FOLDER,
