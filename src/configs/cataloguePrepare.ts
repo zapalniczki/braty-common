2fa63d9da3cf7a458cd66d3fe345db8bc6721a96
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-02-11 16:48:04 +0100

    misc

--- src/configs/cataloguePrepare.ts
+++ src/configs/cataloguePrepare.ts
@@ -3,9 +3,9 @@ import { SCRIPTS } from '..'
 const OUTPUT_BASE_PATH =
   `C:/Users/Przemek Rychlewicz/AppData/Roaming/Adobe/InDesign/Version 16.0/en_GB/Scripts/Scripts Panel/${SCRIPTS.CATALOGUE}` as const
 
-const OUTPUT_IMAGES_FOLDER = `/src/externalAssets` as const
-const OUTPUT_PRODUCTS_FILE_PATH = `/index.ts` as const
-const OUTPUT_CONSTANTS_FILE_PATH = `/src/constants.ts` as const
+const OUTPUT_IMAGES_FOLDER = '/src/externalAssets' as const
+const OUTPUT_PRODUCTS_FILE_PATH = '/src/index.ts' as const
+const OUTPUT_CONSTANTS_FILE_PATH = '/src/constants.ts' as const
 
 export const CATALOGUE_PREPARE_PATHS = {
   OUTPUT_BASE_PATH,
