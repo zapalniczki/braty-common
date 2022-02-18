638f861dc6aff9f32774f1d102fab9f4e132136f
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-02-18 20:40:12 +0100

    Build

--- dist/assets/index.d.ts
+++ dist/assets/index.d.ts
@@ -2,4 +2,4 @@ import { Language } from '..';
 export declare const BRATY_LOGO = "https://firebasestorage.googleapis.com/v0/b/zapalniczki-c7234.appspot.com/o/assets%2Fbraty_logo.png?alt=media&token=2f5017c8-b8ad-4b29-af3f-d6b8b18bd1d2";
 export declare const BRATY_WEBSITE_WORKFLOW_BADGE = "https://github.com/zapalniczki/zapalniczki.github.io/actions/workflows/gh.yaml/badge.svg";
 export declare const BRATY_CATALOGUE_LINK: Record<Language, string>;
-export declare const BRATY_CATALOGUE_COVERS: Record<Language, string>;
+export declare const BRATY_CATALOGUE_COVER: Record<Language, string>;
--- dist/assets/index.js
+++ dist/assets/index.js
@@ -1,13 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.BRATY_CATALOGUE_COVERS = exports.BRATY_CATALOGUE_LINK = exports.BRATY_WEBSITE_WORKFLOW_BADGE = exports.BRATY_LOGO = void 0;
+exports.BRATY_CATALOGUE_COVER = exports.BRATY_CATALOGUE_LINK = exports.BRATY_WEBSITE_WORKFLOW_BADGE = exports.BRATY_LOGO = void 0;
 exports.BRATY_LOGO = 'https://firebasestorage.googleapis.com/v0/b/zapalniczki-c7234.appspot.com/o/assets%2Fbraty_logo.png?alt=media&token=2f5017c8-b8ad-4b29-af3f-d6b8b18bd1d2';
 exports.BRATY_WEBSITE_WORKFLOW_BADGE = 'https://github.com/zapalniczki/zapalniczki.github.io/actions/workflows/gh.yaml/badge.svg';
 exports.BRATY_CATALOGUE_LINK = {
     pl: 'https://drive.google.com/file/d/1CIz5igpUVLTOQ0t8XeCxAHe1SmeLvsBG/view?usp=sharing',
     en: 'https://drive.google.com/file/d/1citVGy1PCq2qRL_Mpv8EtiKhsQ4jXeMx/view?usp=sharing'
 };
-exports.BRATY_CATALOGUE_COVERS = {
+exports.BRATY_CATALOGUE_COVER = {
     pl: 'https://firebasestorage.googleapis.com/v0/b/zapalniczki-c7234.appspot.com/o/assets%2Fcatalogue_winter_2021_2022_cover.webp?alt=media&token=06326030-c010-499a-bff5-528806bf8a8b',
     en: 'https://firebasestorage.googleapis.com/v0/b/zapalniczki-c7234.appspot.com/o/assets%2Fcatalogue_winter_2021_2022_cover.webp?alt=media&token=06326030-c010-499a-bff5-528806bf8a8b'
 };
