2f8acdb87486fad24419de80b678a1ac3be115cb
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-02-18 20:00:26 +0100

    Added assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,17 @@
+import { Language } from '..'
+
 export const BRATY_LOGO =
   'https://firebasestorage.googleapis.com/v0/b/zapalniczki-c7234.appspot.com/o/assets%2Fbraty_logo.png?alt=media&token=2f5017c8-b8ad-4b29-af3f-d6b8b18bd1d2'
 
 export const BRATY_WEBSITE_WORKFLOW_BADGE =
   'https://github.com/zapalniczki/zapalniczki.github.io/actions/workflows/gh.yaml/badge.svg'
+
+export const BRATY_CATALOGUE_LINK: Record<Language, string> = {
+  pl: 'https://drive.google.com/file/d/1CIz5igpUVLTOQ0t8XeCxAHe1SmeLvsBG/view?usp=sharing',
+  en: 'https://drive.google.com/file/d/1citVGy1PCq2qRL_Mpv8EtiKhsQ4jXeMx/view?usp=sharing'
+} as const
+
+export const BRATY_CATALOGUE_COVERS: Record<Language, string> = {
+  pl: 'https://firebasestorage.googleapis.com/v0/b/zapalniczki-c7234.appspot.com/o/assets%2Fcatalogue_winter_2021_2022_cover.webp?alt=media&token=06326030-c010-499a-bff5-528806bf8a8b',
+  en: 'https://firebasestorage.googleapis.com/v0/b/zapalniczki-c7234.appspot.com/o/assets%2Fcatalogue_winter_2021_2022_cover.webp?alt=media&token=06326030-c010-499a-bff5-528806bf8a8b'
+} as const
