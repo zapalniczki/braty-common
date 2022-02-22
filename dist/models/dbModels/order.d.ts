89426bc75a7bafd937724ad7708a6497f18df43d
Przemysław Rychlewicz <Przemyslaw.Rychlewicz@gnb.pl>
2022-02-22 20:06:49 +0100

    Build

--- dist/models/dbModels/order.d.ts
+++ dist/models/dbModels/order.d.ts
@@ -1,5 +1,5 @@
 import { TypeOf } from 'zod';
-declare const orderStatus: import("zod").ZodEnum<["OPEN", "CONFIRMED", "PAYMENT_RECEIVED", "PRODUCTION", "AWAITING_FOR_PICKUP", "IN_DELIVERY", "COMPLETED", "REJECTED"]>;
+export declare const orderStatus: import("zod").ZodEnum<["OPEN", "CONFIRMED", "PAYMENT_RECEIVED", "PRODUCTION", "AWAITING_FOR_PICKUP", "IN_DELIVERY", "COMPLETED", "REJECTED"]>;
 export declare type OrderStatus = TypeOf<typeof orderStatus>;
 export declare const ORDER_STATUSES: ["OPEN", "CONFIRMED", "PAYMENT_RECEIVED", "PRODUCTION", "AWAITING_FOR_PICKUP", "IN_DELIVERY", "COMPLETED", "REJECTED"];
 declare const order: import("zod").ZodObject<import("zod").extendShape<import("zod").extendShape<{
--- dist/models/dbModels/order.js
+++ dist/models/dbModels/order.js
@@ -3,12 +3,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.ORDER_STATUSES = void 0;
+exports.ORDER_STATUSES = exports.orderStatus = void 0;
 var tableBase_1 = __importDefault(require("./tableBase"));
 var zod_1 = require("zod");
 var voucher_1 = __importDefault(require("./voucher"));
 var parcel_1 = __importDefault(require("./parcel"));
-var orderStatus = (0, zod_1.enum)([
+exports.orderStatus = (0, zod_1.enum)([
     'OPEN',
     'CONFIRMED',
     'PAYMENT_RECEIVED',
@@ -18,12 +18,12 @@ var orderStatus = (0, zod_1.enum)([
     'COMPLETED',
     'REJECTED'
 ]);
-exports.ORDER_STATUSES = orderStatus.options;
+exports.ORDER_STATUSES = exports.orderStatus.options;
 var order = tableBase_1.default.extend({
     delivery_type: (0, zod_1.string)().uuid(),
     payment_type: (0, zod_1.string)().uuid(),
     shipping_id: (0, zod_1.string)().uuid().nullable(),
-    status: orderStatus,
+    status: exports.orderStatus,
     total_pl: (0, zod_1.number)(),
     total_en: (0, zod_1.number)(),
     products_price_pl: (0, zod_1.number)(),
